0cf38e49ac67c69fb1be0ae2f9b7b228bd4f561a
Michael Wayne Hoeft & Debbie Balbo <student@epicodus.com>
2015-04-22 14:26:19 -0700

    It fetches a scrabble score from an inputted word

--- spec/scrabbleapp_spec.rb
+++ spec/scrabbleapp_spec.rb
@@ -2,13 +2,13 @@ require('rspec')
 require('scrabbleapp')
 
 describe('String#scrabbleapp') do
-  it("fetches a scrabble for an inputted letter") do
+  it("fetches a scrabble score for an inputted letter") do
   expect(("A").scrabbleapp()).to(eq(1))
 end
 end
 
 describe('String#scrabbleapp') do
-  it("turns the word into a total score") do
+  it("fetches a total score for an inputted word") do
     expect(("ID").scrabbleapp()).to(eq(3))
 end
 end
