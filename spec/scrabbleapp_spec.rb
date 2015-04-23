1a7c54dea15ed14098950be78552196ff2a1cb8d
mikewayne <mhoeft1@my.chemeketa.edu>
2015-04-23 15:58:48 -0700

    Update scrabbleapp_spec.rb

--- spec/scrabbleapp_spec.rb
+++ spec/scrabbleapp_spec.rb
@@ -2,11 +2,11 @@ require('rspec')
 require('scrabbleapp')
 
 describe('String#scrabbleapp') do
-  it("fetches a scrabble score for an inputted letter") do
+  it("fetches a scrabble score for an inputted capital letter") do
   expect(("A").scrabbleapp()).to(eq(1))
 end
 end
 
-  it("fetches a total score for an inputted word") do
+  it("fetches a total score for an inputted word that is in all caps") do
     expect(("ID").scrabbleapp()).to(eq(3))
 end
